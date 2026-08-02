414d6b472e58713beeb53b7160f9c2a5ed0e9b59
Artem Yumina <s25316@pjwstk.edu.pl>
2026-08-03 00:47:14 +0200

    Fix  Educations

--- Frontend/Frontend/Components/Profiles/Layout/Educations/EditEducationFormModel.cs
+++ Frontend/Frontend/Components/Profiles/Layout/Educations/EditEducationFormModel.cs
@@ -1,12 +1,18 @@
 ﻿using Frontend.Components.Shared;
+using System.ComponentModel.DataAnnotations;
 
 namespace Frontend.Components.Profiles.Layout.Educations;
 
 [ValidEducationPeriod]
 public class EditEducationFormModel
 {
+    [Required(ErrorMessage = "Rok startu jest wymagany.")]
     public int StartYear { get; set; }
+
+    [Required(ErrorMessage = "Semestr startu jest wymagany.")]
+    [Range(1, int.MaxValue, ErrorMessage = "Wybierz poprawny semestr startowy.")]
     public int StartSemestrId { get; set; }
+
     public int? EndYear { get; set; }
     public int? EndSemestrId { get; set; }
 }
