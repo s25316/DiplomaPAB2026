d5ceecfd287767eb910dcf8e271cd10d8c533068
Artem Yumina <s25316@pjwstk.edu.pl>
2026-08-03 21:28:25 +0200

    maked uniwersal UI for profile

---
+++ Frontend/Frontend/Extensions/AuthenticationStateExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Frontend.Extensions;
+
+public static class AuthenticationStateExtensions
+{
+    public static string? GetPersonId(
+        this AuthenticationState state
+    ) => state.User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
+}
